eb64d36a34a6bd3d97c4c6de41ade3003e2976be
Doug <doug@hsdirect.co.uk>
2016-02-22 16:16:08 +0000

    Init2

--- Backend/vendor/composer/autoload_namespaces.php
+++ Backend/vendor/composer/autoload_namespaces.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'SolutionHost\\Tests' => array($baseDir . '/tests'),
+    'SolutionHost' => array($baseDir . '/src'),
 );
