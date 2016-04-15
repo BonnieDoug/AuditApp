8e985eb0f83d10c87af2f2e8df38a4580a1530ab
BonnieDoug <doug@bonniechef.com>
2016-04-15 13:46:57 +0100

    this

--- client/assets/js/services/AuthChecker.js
+++ client/assets/js/services/AuthChecker.js
@@ -1,6 +1,10 @@
-app.factory("AuthChecker", ['$http', '$window',
-    function ($http, $window) { // This service connects to our REST API
+// This service controls our auth tokens. Stored on filesystem rather than in 
+// Cookies or Sessions as we can't use PHP sessions from the front end and we 
+// can't rely on Sessions/Cookies when/if moved to a mobile native app. 
+// Plus Session and Cookies are so old school...
 
+app.factory("AuthChecker", ['$window',
+    function ($window) { 
         var self = {};
         self.parseJwt = function (token) {
             var base64Url = token.split('.')[1];
@@ -19,27 +23,28 @@ app.factory("AuthChecker", ['$http', '$window',
                 console.log('JWT:', token);
             }
             return res.data.message;
-        }
+        };
 
         self.isAuthed = function () {
-           
+
             var token = self.getToken();
             if (token) {
                 var params = self.parseJwt(token);
+                //Convert PHP Unix Seconds to JS Unix Milliseconds then check if expired.
                 return Math.round(new Date().getTime() / 1000) <= params.exp;
             } else {
                 return false;
             }
-        }
+        };
 
         self.logout = function () {
             $window.localStorage.removeItem('jwtToken');
-        }
+        };
 
 
-        self.getParsedToken = function(){
+        self.getParsedToken = function () {
             return self.parseJwt(self.getToken());
-        }
+        };
 
         return self;
     }]);
